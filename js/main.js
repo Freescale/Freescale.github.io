6ce142f7433945b4cfeaa09b5de36136ca2907af
Miguel Satte Alam Lisboa <miguellisboa@gmail.com>
2014-02-26 13:29:30 -0300

    Added google analytics
    Signed-off-by: Miguel Satte Alam Lisboa <miguellisboa@gmail.com>

--- js/main.js
+++ js/main.js
@@ -161,6 +161,15 @@ $('document').ready(function() {
 
     /************************************* Initializes the application *************************************/
 
+    // Added google analytics
+    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                            })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+    ga('create', 'UA-48436418-1', 'freescale.github.io');
+    ga('send', 'pageview');
+
     if(!$isMobile()) {
         $updateNavbarAndBackButton();
         $handlerHeaderSection.updateHeight($window.height(), $handlerMainSection.getOuterHeight());
@@ -212,5 +221,4 @@ $('document').ready(function() {
     });
 
     /*******************************************************************************************************/
-
 });

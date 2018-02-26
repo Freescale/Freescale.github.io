6e5f50268fcf52fc31e317e1348e8c8734c82154
Otavio Salvador <otavio@ossystems.com.br>
2018-02-26 09:57:04 -0300

    Update doc/release-notes/2.4
    Signed-off-by: Otavio Salvador <otavio@ossystems.com.br>

--- doc/release-notes/2.4/_static/documentation_options.js
+++ doc/release-notes/2.4/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '2.4',
+    VERSION: '2.4 (Draft document)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',

1f41424c40ccc85a6336a6e1a4a0fb34aa4aebe3
Daiane Angolini <daiane.angolini@foundries.io>
2023-12-29 15:55:25 -0300

    index: Update the GH pages
    Update the page to reflect the project changes.
    
    After https://github.com/Freescale/Documentation/pull/42 the
    Documentation is deprecated.
    
    Add the link to the current Yocto Project releases.
    
    Suggest downloading the master branch, so this page is always up to
    date.
    
    Remove the Documentation files (pdfs) and the script to update the site.
    
    Signed-off-by: Daiane Angolini <daiane.angolini@foundries.io>

--- doc/release-notes/2.3/_static/documentation_options.js
+++
@@ -1,9 +0,0 @@
-var DOCUMENTATION_OPTIONS = {
-    URL_ROOT: '',
-    VERSION: '2.3',
-    LANGUAGE: 'None',
-    COLLAPSE_INDEX: false,
-    FILE_SUFFIX: '.html',
-    HAS_SOURCE: false,
-    SOURCELINK_SUFFIX: '.txt'
-};
